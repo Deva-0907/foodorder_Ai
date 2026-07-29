cd6bbf98adc8662e09df87392d81b13552bd010b
Deva-0907 <thiyagarajanivethitha@gmail.com>
2026-07-29 16:56:59 +0530

    fix: warning in app.py

---
+++ llm/openrouter.py
@@ -0,0 +1,26 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+client = OpenAI(
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
+)
+
+
+def ask_openrouter(prompt):
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role":"user",
+                "content":prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
