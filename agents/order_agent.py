f9d56a4884b2e8fc005d5d2579173315e9a65636
Deva-0907 <thiyagarajanivethitha@gmail.com>
2026-07-29 14:05:28 +0530

    connect the app.py

---
+++ agents/order_agent.py
@@ -0,0 +1,23 @@
+from llm.openrouter import ask_openrouter
+
+
+class OrderAgent:
+
+    def create_order(self, recommendation):
+
+        prompt = f"""
+Create a professional food order summary.
+
+Recommendation:
+
+{recommendation}
+
+Include:
+
+- Ordered Item
+- Estimated Price
+- Quantity
+- Thank You Message
+"""
+
+        return ask_openrouter(prompt)
